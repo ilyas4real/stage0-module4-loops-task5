13e54f2ef25185e1823080c4ad0396fb990c47c6
ilyas4real <iliyas.ysmaiyl@gmail.com>
2023-02-07 20:48:43 +0100

    done

--- src/main/java/school/mjc/stage0/loops/task5/Rectangle.java
+++ src/main/java/school/mjc/stage0/loops/task5/Rectangle.java
@@ -2,13 +2,13 @@ package school.mjc.stage0.loops.task5;
 
 public class Rectangle {
     public void printRectangleFrom8s(int length, int height){
-        for(int i = 1; i <= length; i++) {
-            for(int j = 1; j <= height; j++) {
-                if (i == 1 || i == length || j == 1 || j == height) {
-                    System.out.print("8");
-                } else {
-                    System.out.print(" ");
-                }
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < length; j++) {
+                if (i == 0 || j == length - 1)
+                    System.out.print(8);
+                else if (j == 0 || i == height -1)
+                    System.out.print(8);
+                else System.out.print(" ");
             }
             System.out.println();
         }
